0bb97434b6aaea8637263cb1848d0890770d37fd
Ankit Raj <ankitsingh17072001@gmail.com>
2023-09-17 08:11:44 +0530

    Api for register is created successfully

---
+++ controller/userController.js
@@ -0,0 +1,20 @@
+const User = require('../models/userModel')
+const asyncHandler =require('express-async-handler')
+
+
+const createUser = asyncHandler(async(req,res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({email: email});
+    if (!findUser){
+        //create new user
+        const newUser =await User.create(req.body);
+        res.json(newUser);
+        console.log(newUser);
+    }
+    else{
+        //user already exists
+        throw new Error("User already exists")
+    }
+})
+
+module.exports = {createUser}
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require ('express');
+const bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const dbConnect = require('./config/dbConnect');
+const { notFound,errorHandler } = require('./middlewares/errorHandler');
 const PORT = process.env.PORT || 4000;
+// const authRoute = require('./routes/authRoutes')
 
 //configuration of database and calling it form config
 dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/', (req,res) => {
-    res.send("Welcome to Ecommerce Application")
-});
+app.use('/api/user', require('./routes/authRoutes'))
 
-app.listen(PORT,() => {
+app.use(notFound);
+app.use(errorHandler);
+
+// app.use('/', (req, res) => {
+//     res.send("Welcome to Ecommerce Application")
+// });
+
+app.listen(PORT, () => {
     console.log(`Server is running at Port ${PORT}`);
 })   
